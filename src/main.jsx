b3e678f50624e8183119b54adf2052f5acd16097
ajopi <fachrisajovi@gmail.com>
2025-01-23 01:14:24 +0700

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router";
 import RouterDefault from "./routes/RouterDefault.jsx";
 
 const queryClient = new QueryClient();
